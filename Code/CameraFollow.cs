9621fb4f81d087ee909c2855eb7949ab3d8e1981
(Michelle) Willa Qu <michelle.w.qu@gmail.com>
2019-10-22 16:51:06 -0700

    Backing up

---
+++ Code/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothspeed = 0.25f;
+    public Vector3 offset;
+
+
+     void LateUpdate()
+    {
+
+        transform.position = target.position + offset ; 
+    }
+
+
+
+
+
+}
